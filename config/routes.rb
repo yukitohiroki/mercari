26eb5214d344f40a0cca5c30c1c17d0057eea810
44Kuniy <yoshikuni3104@gmail.com>
2019-03-31 12:49:17 +0900

    Route debug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
 
 end

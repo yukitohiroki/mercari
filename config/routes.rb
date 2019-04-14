33002f58fa73062ef30fe0311b7701187ec53365
44Kuniy <yoshikuni3104@gmail.com>
2019-04-14 15:37:21 +0900

    Routes my page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resource :users, only: [:edit, :update]
+  get '/mypage', to: 'mypage#index'
   resource :mypage, only: [:index] do
     resource :profile, only: [:edit]
   end

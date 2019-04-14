54911226100ab34729e801ee8763488b1942a1e1
44Kuniy <yoshikuni3104@gmail.com>
2019-04-14 16:05:58 +0900

    Route resolution

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
-  resource :users, only: [:edit, :update]
+
+  resource :users, only: [:edit, :update] do
+    get '/logout', to:'users#logout'
+  end
+
   get '/mypage', to: 'mypage#index'
   resource :mypage, only: [:index] do
     resource :profile, only: [:edit]
